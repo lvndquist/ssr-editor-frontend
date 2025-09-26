c01e64dbebae10e48e6c1b8cea2c83c93db650bd
lvndquist <jonatanlundqvistsilins@gmail.com>
2025-09-26 02:45:45 +0200

    add link component

--- src/components/index/AddButton.jsx
+++ src/components/index/AddButton.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { Link } from 'react-router-dom';
 
 export default function AddButton() {
     return (
