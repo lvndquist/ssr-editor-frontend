19d1de8bfa774bb329095138d3ba9eb19209aa9e
Jonatan <lvndquist@gmail.com>
2025-10-28 19:18:49 +0000

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename='/ssr-editor-frontend'>
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>
