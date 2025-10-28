518b69253bd2f5606bb00279858f1f00affd820b
Jonatan <lvndquist@gmail.com>
2025-10-28 18:40:46 +0000

    backend linked

--- src/components/index/Register.jsx
+++ src/components/index/Register.jsx
@@ -41,7 +41,6 @@ export default function Register() {
     }
 
     async function register(email, password) {
-        console.log(apiUrl)
         const res = await fetch(`${apiUrl}/register`, {
             method: "POST",
             headers: {
